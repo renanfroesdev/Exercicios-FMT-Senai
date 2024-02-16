a1015595013d01394ccbf475065fe54eb8909857
Renan Fróes <151413922+renanfroesdev@users.noreply.github.com>
2024-02-16 11:12:37 -0300

    resolução ex03

---
+++ exercicios_trello/ex03-concatenacao/script.js
@@ -0,0 +1,8 @@
+// Lista de Exercícios do Trello - Aluno: Renan dos Santos
+// Exercicio 3 - Combine duas strings usando o operador de concatenação 
+// e exiba o resultado no console.
+// Utilize o template string ou a concatenação simples.
+
+let string1 = 'Abacaxi'
+let string2 = 'Morango'
+console.log(`Exemplo de Concatenação: ${string1} e ${string2} são Frutas!`)
