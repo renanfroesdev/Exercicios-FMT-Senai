36065f15b0c41c9ef94df23d64294f542baf03e5
Renan Fróes <151413922+renanfroesdev@users.noreply.github.com>
2024-02-16 12:12:44 -0300

    resolucão ex04

---
+++ exercicios_trello/ex04-funcão_param/script.js
@@ -0,0 +1,11 @@
+// Lista de Exercícios do Trello - Aluno: Renan dos Santos
+// Exercicio 4 - Crie uma função que receba dois números 
+// como parâmetros e retorne sua soma no console.log.
+
+function soma(a, b) {
+    let num1 = parseFloat(prompt('Insira um número: '))
+    let num2 = parseFloat(prompt('Insira outro Número: '))
+    resultado = num1 + num2
+    console.log(`o resultado da soma é ${resultado}`)
+}
+
