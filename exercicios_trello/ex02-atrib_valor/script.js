e8a769bdd78b6e03761d6048324801e863503c10
Renan Fróes <151413922+renanfroesdev@users.noreply.github.com>
2024-02-16 10:58:53 -0300

    resolução ex02

---
+++ exercicios_trello/ex02-atrib_valor/script.js
@@ -0,0 +1,7 @@
+// Lista de Exercícios do Trello - Aluno: Renan dos Santos
+// Exercicio 2 - Declare uma variável e atribua um número a ela, 
+// em seguida, exiba seu valor no console.
+// Atribua o valor a variável usando o window.prompt();
+
+let variavel = prompt('Digite um valor para a variável: ')
+console.log(`"${variavel}", foi digitado`)
