8345f6526f0fd2dc50e0d942d5887e2e08f4caa6
Renan Fróes <151413922+renanfroesdev@users.noreply.github.com>
2024-02-16 12:29:30 -0300

    resolucão ex05

---
+++ exercicios_trello/ex05-estrut_cond/script.js
@@ -0,0 +1,13 @@
+// Lista de Exercícios do Trello - Aluno: Renan dos Santos
+// Exercicio 5 - Utilize uma estrutura condicional (if,else) 
+// para verificar se um número é positivo, negativo ou zero. 
+// Exiba os valores no console.log()
+
+let num = prompt('Digite um número para verificar: ')
+if (num == 0) {
+    console.log('O número digitado é Zero')
+}else if (num > 0){
+    console.log('O número digitado é Positivo')
+}else {
+    console.log('O número digitado é Negativo')
+}
