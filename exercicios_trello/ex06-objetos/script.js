804e13bf0138d9fcfd79e1434ea7b5eb8542cbe5
Renan Fróes <151413922+renanfroesdev@users.noreply.github.com>
2024-02-16 12:53:01 -0300

    resolucão ex06

---
+++ exercicios_trello/ex06-objetos/script.js
@@ -0,0 +1,27 @@
+// Lista de Exercícios do Trello - Aluno: Renan dos Santos
+// Exercicio 6 - Crie 3 objetos com propriedades representando informações pessoais
+// (nome, idade, nacionalidade e profissão) e exiba essas informações 
+// com um console.log para cada objeto.
+
+let pessoa1 = {
+    nome: 'Carlos Alberto',
+    idade: 23,
+    nacionalidade:'brasileiro',
+    profissão:'motorista' ,
+}
+let pessoa2 = {
+    nome: 'Katia Regina',
+    idade: 39,
+    nacionalidade:'brasileira',
+    profissão:'Aposentada' ,
+}
+let pessoa3 = {
+    nome: 'Lucas Alfredo',
+    idade: 29,
+    nacionalidade:'brasileiro',
+    profissão:'empresario' ,
+}
+
+console.log(pessoa1)
+console.log(pessoa2)
+console.log(pessoa3)
